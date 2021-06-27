8cb06fca87bdf801bccb09d93e7049a29bc98de0
Mohammed506 <81885137+Mohammed506@users.noreply.github.com>
2021-06-27 03:48:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -245,10 +245,7 @@ def create_venue_submission():
 
 
     # TODO: insert form data as a new Venue record in the db, instead
-#    form = VenueForm(request.form)
-#    if not form.validate():
-#         print(form.errors)
-#         return render_template('forms/new_venue.html', form=form)
+
 
     
    try:
@@ -343,14 +340,7 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    # response = {
-    #     "count": 1,
-    #     "data": [{
-    #         "id": 4,
-    #         "name": "Guns N Petals",
-    #         "num_upcoming_shows": 0,
-    #     }]
-    # }
+
     search_term=request.form.get('search_term', '')
     search_art = Artist.query.filter(Artist.name.ilike(f'%{search_term}%'))
     response = { 
